b6f357d2f659a791d3ce8493b5723758683ed0fa
Lennart Regebro <lregebro@redhat.com>
2014-10-27 22:09:33 +0100

    Preparing release 0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_desc = readme + '\n\n' + changes
 
 setup(
     name='spiny',
-    version='0.2.dev0',
+    version='0.2',
     description='''Spiny will run your Python tests under multiple versions of Python''',
     long_description=long_desc,
     keywords=['development', 'tools', 'testing'],
