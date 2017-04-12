37830005714edce77c5b41694bea2b3346b01c73
Lennart Regebro <regebro@gmail.com>
2017-04-12 21:59:22 +0200

    Back to development: 0.7

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_desc = readme + '\n\n' + changes
 
 setup(
     name='spiny',
-    version='0.6',
+    version='0.7.dev0',
     description='''Spiny will run your Python tests under multiple versions of Python''',
     long_description=long_desc,
     keywords=['development', 'tools', 'testing'],
